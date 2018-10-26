01c68b10dacc2bd999c23550fab17f4e45dfc70d
will <wherding@asu.edu>
2018-10-25 20:46:36 -0700

    fianl

--- server.js
+++ server.js
@@ -51,7 +51,9 @@ let waitlist = [{
     id:7
 }];
 
-
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "public/home.html"));
+});
 app.get("/home", function(req, res){
     res.sendFile(path.join(__dirname, "public/home.html"));
 });
